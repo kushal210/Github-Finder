95740d9553b6d1ee7758f49de2d25eb5ea59c1a2
kushal <kushalagarwal210@gmail.com>
2020-10-15 21:45:00 +0530

    Project without using React-Hooks

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h2>About</h2>
+            <p>Search Github Users...</p>
+        </Fragment>
+    )
+}
+export default About;
